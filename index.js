d5678691533ea56572cbe37ad377ab9abae1f254
CrackerJaps <seantokuzo@gmail.com>
2022-02-14 21:50:18 -0800

    move characters variable into index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import characters from '/characters.js'
+const characters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '!', '@', '#', '$', '%', '^', '&', '*', '<', '>', '?', '-', '+', '='
+]
 
 const inputOne = document.getElementById('input-1')
 const inputTwo = document.getElementById('input-2')
